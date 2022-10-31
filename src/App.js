5f74f91656f369166e26aeba45a19f45929d0af7
AllyB <allendebennett@gmail.com>
2022-10-31 17:17:27 -0500

    tidied up files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather search Engine</h1>
+      <h1>Weather Search Engine</h1>
       <WeatherEngine />
     </div>
   );
